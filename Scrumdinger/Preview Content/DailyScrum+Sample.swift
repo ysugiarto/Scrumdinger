fc40cf2617d0bd36e1001c66d239ca82a26429af
ysugiarto <yoh.sugiarto@gmail.com>
2025-05-09 05:19:34 +0700

    Add list and card for the list

---
+++ Scrumdinger/Preview Content/DailyScrum+Sample.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum+Sample.swift
+//  Scrumdinger
+//
+//  Created by Yohanes Sugiarto on 08/05/25.
+//
+
+import Foundation
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,8 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Yohanes Sugiarto on 08/05/25.
+//
+
+import Foundation
