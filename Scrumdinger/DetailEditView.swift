39c9c57545af632dc498c499411ae6978fd1975a
ysugiarto <yoh.sugiarto@gmail.com>
2025-06-20 10:07:33 +0700

    Update Views

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Yohanes Sugiarto on 09/05/25.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Yohanes Sugiarto on 09/05/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
